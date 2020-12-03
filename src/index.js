bb28381dce85981caf3f28e79292590e84b4ddd2
Anjali <anjali170601@gmail.com>
2020-12-03 22:44:27 +0530

    in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port ||  3000
+const port = process.env.PORT ||  3000
 const path =require('path')
 const Filter = require('bad-words')
 const { addUser , removeUser , getUser, getUsersInRoom  } = require('./utils/users.js')
